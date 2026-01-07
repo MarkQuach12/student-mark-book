6d0c86766ee77ebfc9798f6409d2c9e96e21e345
Mark Quach <mark.quach1234@gmail.com>
2026-01-08 00:20:40 +1100

    Added basic routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
 import "./App.css";
 import Navbar from "./components/navbar";
+import LandingPage from "./pages/landingPage";
+import { Routes, Route } from "react-router-dom";
+import ProfilePage from "./pages/profilePage";
+import ClassPage from "./pages/classPage";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/classOverview/:id" element={<ClassPage />} />
+      </Routes>
     </>
   );
 }
---
+++ frontend/src/pages/classPage.tsx
@@ -0,0 +1,10 @@
+const ClassPage = () => {
+
+    return (
+        <div>
+            <h1>Class Page</h1>
+        </div>
+    )
+}
+
+export default ClassPage;
